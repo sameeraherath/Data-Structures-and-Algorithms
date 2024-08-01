75ba7ca8e3e6c7f3ab07367b3093f71c734e3c9d
sameeraherath <sameerastar6@gmail.com>
2024-08-01 10:55:54 +0530

    Update File

---
+++ arrays/ArrayOperations.java
@@ -0,0 +1,60 @@
+public class ArrayOperations {
+
+    public static void main(String[] args) {
+
+        //initialize array
+        int [] numbers = {1, 2, 3, 4, 5};
+
+        //print array
+        System.out.println("Array Elements: ");
+        printArray(numbers);
+
+        //Find the maximum value in the array
+        System.out.println("Maximum Value: " + findMaxValue(numbers));
+
+        //calculate sum of the array
+        System.out.println("Sum : " + calculateSum(numbers));
+
+
+    }
+
+    //Method to print array
+    public static void printArray(int[] numbers) {
+
+        for(int value: numbers) {
+
+            System.out.println(value);
+
+      }
+
+    }
+
+    //Method to find the maximum value in the array
+    public static int findMaxValue(int[] array) {
+
+        int max = array[0];
+        for (int value: array) {
+
+            if (value > max) {
+
+                max = value;
+            }
+
+        }
+
+        return max; 
+    }
+
+    public static int calculateSum(int[] array) {
+
+    int sum = 0;
+    for (int value: array) {
+    
+        sum += value;
+    }
+
+    return sum;
+
+  }
+
+}
